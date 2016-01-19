9b2046ba6ec44b586ba9e86cf3e66cc396a533ff
Stefan Schulze <stefan.schulze@pentasys.de>
2016-01-19 17:48:34 +0100

    Add integration test for SimpleTemplateEngine

--- src/test/java/workshop/panda/birthday/templating/SimpleTemplateEngineIT.java
+++ src/test/java/workshop/panda/birthday/templating/SimpleTemplateEngineIT.java
@@ -1,10 +1,29 @@
 package workshop.panda.birthday.templating;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+import workshop.panda.birthday.core.TemplateEngine;
 
 /**
  * Created by schulzst on 19.01.2016.
  */
 public class SimpleTemplateEngineIT {
 
+    private TemplateEngine underTest = new SimpleTemplateEngine("{number} bottles of {drink}");
+
+    @Test
+    public void variablesReplaced() throws Exception {
+        Map<String, String> variables = new HashMap<>();
+        variables.put("number", "99");
+        variables.put("drink", "beer");
+
+        String result = underTest.replaceInTemplate(variables);
+
+        assertThat("Filled template", result, is("99 bottles of beer"));
+    }
 }
