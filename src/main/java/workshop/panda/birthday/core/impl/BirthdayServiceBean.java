1aa9d14da96b8b2624e1c542fe81cfeb25ded710
Stefan Schulze <stefan.schulze@pentasys.de>
2016-01-30 18:04:23 +0100

    Remove type BirthdayMessage

--- src/main/java/workshop/panda/birthday/core/impl/BirthdayServiceBean.java
+++ src/main/java/workshop/panda/birthday/core/impl/BirthdayServiceBean.java
@@ -2,13 +2,9 @@ package workshop.panda.birthday.core.impl;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.stream.Collectors;
-
 import javax.mail.Message;
 import javax.mail.Session;
 import javax.mail.Transport;
@@ -18,7 +14,6 @@ import javax.mail.internet.MimeMessage;
 import workshop.panda.birthday.core.BirthdayService;
 import workshop.panda.birthday.core.TemplateException;
 import workshop.panda.birthday.core.model.BirthDate;
-import workshop.panda.birthday.core.model.BirthdayMessage;
 import workshop.panda.birthday.core.model.Customer;
 import workshop.panda.birthday.core.model.Gender;
 
@@ -27,30 +22,15 @@ import workshop.panda.birthday.core.model.Gender;
  */
 public class BirthdayServiceBean implements BirthdayService {
 
-    private List<Customer> allCustomers = new ArrayList<>();
+    private String fileName;
 
     private Properties mailProperties = new Properties();
 
     private Properties templates;
 
-    BirthdayServiceBean() {
-    }
-
     public BirthdayServiceBean(String fileName, int smtpPort, String smtpHost, Properties templates)
         throws Exception {
-        BufferedReader in = new BufferedReader(new FileReader(fileName));
-        String line = in.readLine();
-        while ((line = in.readLine()) != null) {
-            String[] rawData = line.split(";");
-            Customer customer = new Customer(
-                rawData[1],
-                rawData[0],
-                new BirthDate(rawData[2]),
-                rawData[3],
-                Gender.valueOf(rawData[4]));
-            allCustomers.add(customer);
-        }
-        in.close();
+        this.fileName = fileName;
         this.mailProperties.put("mail.smtp.host", smtpHost);
         this.mailProperties.put("mail.smtp.port", "" + smtpPort);
         this.templates = templates;
@@ -58,37 +38,43 @@ public class BirthdayServiceBean implements BirthdayService {
 
     @Override
     public void sendGreetings(BirthDate today) throws Exception {
-        List<Customer> customers = allCustomers.stream()
-            .filter(c -> c.hasBirthday(today))
-            .collect(Collectors.toList());
-        for (Customer customer : customers) {
-            Map<String, Object> replacements = new HashMap<>();
-            replacements.put("title", customer.getGender() == Gender.FEMALE ? "Liebe" : "Lieber");
-            replacements.put("name", customer.getFirstName());
-            replacements.put("age", customer.age(today));
-            String result1;
-            if (templates.containsKey("standard")) {
-                String result = templates.getProperty("standard");
-                for (Map.Entry<String, Object> entry : replacements.entrySet()) {
-                    result = result.replace("{" + entry.getKey() + "}", entry.getValue().toString());
+        BufferedReader in = new BufferedReader(new FileReader(fileName));
+        try {
+            String line = in.readLine();
+            while ((line = in.readLine()) != null) {
+                String[] rawData = line.split(";");
+                Customer customer = new Customer(
+                    rawData[1],
+                    rawData[0],
+                    new BirthDate(rawData[2]),
+                    rawData[3],
+                    Gender.valueOf(rawData[4]));
+
+                if (customer.hasBirthday(today)) {
+                    Map<String, Object> replacements = new HashMap<>();
+                    replacements.put("title", customer.getGender() == Gender.FEMALE ? "Liebe" : "Lieber");
+                    replacements.put("name", customer.getFirstName());
+                    replacements.put("age", customer.age(today));
+                    if (templates.containsKey("standard")) {
+                        String body = templates.getProperty("standard");
+                        for (Map.Entry<String, Object> entry : replacements.entrySet()) {
+                            body = body.replace("{" + entry.getKey() + "}", entry.getValue().toString());
+                        }
+                        Session session = Session.getInstance(mailProperties, null);
+                        Message message = new MimeMessage(session);
+                        message.setFrom(new InternetAddress("vertrieb@company.de"));
+                        message.setRecipient(Message.RecipientType.TO, new InternetAddress(customer.getEmailAddress()));
+                        message.setSubject("Alles Gute zum Geburtstag!");
+                        message.setText(body);
+                        Transport.send(message);
+                    } else {
+                        throw new TemplateException("No template found with id 'standard'");
+                    }
                 }
-                result1 = result;
-            } else {
-                throw new TemplateException("No template found with id '" + "standard" + "'");
+
             }
-            BirthdayMessage birthdayMessage = new BirthdayMessage(
-                "vertrieb@company.de",
-                customer.getEmailAddress(),
-                "Alles Gute zum Geburtstag!",
-                result1
-            );
-            Session session = Session.getInstance(mailProperties, null);
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(birthdayMessage.getFrom()));
-            message.setRecipient(Message.RecipientType.TO, new InternetAddress(birthdayMessage.getTo()));
-            message.setSubject(birthdayMessage.getSubject());
-            message.setText(birthdayMessage.getBody());
-            Transport.send(message);
+        } finally {
+            in.close();
         }
     }
 
--- src/test/java/workshop/panda/birthday/core/BirthdayServiceIT.java
+++ src/test/java/workshop/panda/birthday/core/BirthdayServiceIT.java
@@ -6,18 +6,24 @@ import static org.hamcrest.Matchers.*;
 import java.util.Properties;
 import javax.mail.internet.MimeMessage;
 
+import com.icegreen.greenmail.junit.GreenMailRule;
+import com.icegreen.greenmail.util.GreenMailUtil;
+import com.icegreen.greenmail.util.ServerSetup;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import workshop.panda.birthday.GreenmailTestBase;
 import workshop.panda.birthday.core.impl.BirthdayServiceBean;
 import workshop.panda.birthday.core.model.BirthDate;
-import workshop.panda.birthday.core.model.BirthdayMessage;
 
 /**
  * Created by schulzst on 15.01.2016.
  */
-public class BirthdayServiceIT extends GreenmailTestBase {
+public class BirthdayServiceIT {
 
+    protected static final int SMTP_PORT = 3025;
+    protected static final String SMTP_HOST = "127.0.0.1";
+    @Rule
+    public final GreenMailRule greenMail = new GreenMailRule(new ServerSetup(BirthdayServiceIT.SMTP_PORT, BirthdayServiceIT.SMTP_HOST, "smtp"));
     private BirthdayService underTest;
 
     @Before
@@ -36,11 +42,11 @@ public class BirthdayServiceIT extends GreenmailTestBase {
         underTest.sendGreetings(new BirthDate("2016-08-19"));
         MimeMessage[] emails = greenMail.getReceivedMessages();
         assertThat("Number of messages", emails.length, is(1));
-        assertMessage(emails[0], new BirthdayMessage(
-                "vertrieb@company.de",
-                "anastasia.baum@mnet-mail.de",
-                "Alles Gute zum Geburtstag!",
-                "Liebe Anastasia, Zu deinem 38. Geburtstag alles Gute ..."));
+        MimeMessage message = emails[0];
+        assertThat("From", message.getFrom()[0].toString(), is("vertrieb@company.de"));
+        assertThat("To", GreenMailUtil.getAddressList(message.getAllRecipients()), is("anastasia.baum@mnet-mail.de"));
+        assertThat("Subject", message.getSubject(), is("Alles Gute zum Geburtstag!"));
+        assertThat("Body", GreenMailUtil.getBody(message), is("Liebe Anastasia, Zu deinem 38. Geburtstag alles Gute ..."));
     }
 
     @Test
@@ -49,4 +55,5 @@ public class BirthdayServiceIT extends GreenmailTestBase {
         MimeMessage[] emails = greenMail.getReceivedMessages();
         assertThat("Number of messages", emails.length, is(2));
     }
+
 }
