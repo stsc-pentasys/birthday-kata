fb49dacb8f8310336168a400dacceee4e5aa6912
Stefan Schulze <stefan.schulze@pentasys.de>
2016-02-06 10:50:04 +0100

    Extract private methods based on functionality

--- src/main/java/workshop/panda/birthday/core/BirthdayServiceBean.java
+++ src/main/java/workshop/panda/birthday/core/BirthdayServiceBean.java
@@ -2,10 +2,14 @@ package workshop.panda.birthday.core;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -36,44 +40,63 @@ public class BirthdayServiceBean implements BirthdayService {
 
     @Override
     public void sendGreetings(BirthDate today) throws Exception {
+        List<Customer> customers = findCustomersByBirthday(today);
+        for (Customer customer : customers) {
+            render(today, customer);
+        }
+    }
+
+    private List<Customer> findCustomersByBirthday(BirthDate today) throws IOException {
+        List<Customer> customers = new LinkedList<>();
         BufferedReader in = new BufferedReader(new FileReader(fileName));
         try {
             String line = in.readLine();
             while ((line = in.readLine()) != null) {
-                String[] rawData = line.split(";");
-                Customer customer = new Customer(
-                    rawData[1],
-                    rawData[0],
-                    new BirthDate(rawData[2]),
-                    rawData[3],
-                    Gender.valueOf(rawData[4]));
-
+                Customer customer = parseCustomer(line);
                 if (customer.hasBirthday(today)) {
-                    Map<String, Object> replacements = new HashMap<>();
-                    replacements.put("title", customer.getGender() == Gender.FEMALE ? "Liebe" : "Lieber");
-                    replacements.put("name", customer.getFirstName());
-                    replacements.put("age", customer.age(today));
-                    if (templates.containsKey("standard")) {
-                        String body = templates.getProperty("standard");
-                        for (Map.Entry<String, Object> entry : replacements.entrySet()) {
-                            body = body.replace("{" + entry.getKey() + "}", entry.getValue().toString());
-                        }
-                        Session session = Session.getInstance(mailProperties, null);
-                        Message message = new MimeMessage(session);
-                        message.setFrom(new InternetAddress("vertrieb@company.de"));
-                        message.setRecipient(Message.RecipientType.TO, new InternetAddress(customer.getEmailAddress()));
-                        message.setSubject("Alles Gute zum Geburtstag!");
-                        message.setText(body);
-                        Transport.send(message);
-                    } else {
-                        throw new TemplateException("No template found with id 'standard'");
-                    }
+                    customers.add(customer);
                 }
-
             }
         } finally {
             in.close();
         }
+        return customers;
+    }
+
+    private Customer parseCustomer(String line) {
+        String[] rawData = line.split(";");
+        return new Customer(
+            rawData[1],
+            rawData[0],
+            new BirthDate(rawData[2]),
+            rawData[3],
+            Gender.valueOf(rawData[4]));
+    }
+
+    private void render(BirthDate today, Customer customer) throws MessagingException, TemplateException {
+        Map<String, Object> replacements = new HashMap<>();
+        replacements.put("title", customer.getGender() == Gender.FEMALE ? "Liebe" : "Lieber");
+        replacements.put("name", customer.getFirstName());
+        replacements.put("age", customer.age(today));
+        if (templates.containsKey("standard")) {
+            String body = templates.getProperty("standard");
+            for (Map.Entry<String, Object> entry : replacements.entrySet()) {
+                body = body.replace("{" + entry.getKey() + "}", entry.getValue().toString());
+            }
+            send(customer, body);
+        } else {
+            throw new TemplateException("No template found with id 'standard'");
+        }
+    }
+
+    private void send(Customer customer, String body) throws MessagingException {
+        Session session = Session.getInstance(mailProperties, null);
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress("vertrieb@company.de"));
+        message.setRecipient(Message.RecipientType.TO, new InternetAddress(customer.getEmailAddress()));
+        message.setSubject("Alles Gute zum Geburtstag!");
+        message.setText(body);
+        Transport.send(message);
     }
 
 }
